f38edc146a6f6d711149eb05cc9afb90f95f1ae7
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2025-02-13 23:14:28 +0530

    Update Priority.c

--- Scheduling /Non_preemptive /Priority.c
+++ Scheduling /Non_preemptive /Priority.c
@@ -1,105 +1,115 @@
 #include <stdio.h>
-#include <string.h>
+#include <limits.h>
 
-int main() {
-    int n, i, j, min, found, lastScheduled = 0, num = 0, idle = 0;
-    float totalWaitingTime = 0.0, totalTurnAroundTime = 0.0;
-
-    // Arrays for process details
-    char name[20][20], ganttChart[40][20];
-    int arrivalTime[20], burstTime[20], priority[20], status[20], completionTime[20], waitingTime[20], turnAroundTime[20];
-    int startTime[40], endTime[40];
-
-    // Input: Number of processes
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-
-    // Input: Process details
-    for (i = 0; i < n; i++) {
-        printf("\nEnter details of process %d", i + 1);
-        printf("\nProcess Name: ");
-        scanf("%s", name[i]);
-        printf("Arrival Time: ");
-        scanf("%d", &arrivalTime[i]);
-        printf("Burst Time: ");
-        scanf("%d", &burstTime[i]);
-        printf("Priority: ");
-        scanf("%d", &priority[i]);
-        status[i] = 0; // Mark process as not executed yet
-    }
-
-    // Priority Scheduling Execution
-    for (i = 0; lastScheduled < n;) {
-        found = 0;
-
-        // Find the highest priority process that has arrived
-        for (j = 0; j < n; j++) {
-            if (i >= arrivalTime[j] && status[j] == 0) {
-                if (found == 0) {
-                    min = j;
-                    found = 1;
-                } else if ((priority[min] > priority[j]) || (priority[min] == priority[j] && arrivalTime[min] > arrivalTime[j])) {
-                    min = j;
+void findWaitingTime(int processes[], int n, int bt[], float at[], int priority[], int wt[], int ct[], int start[]) {
+    int completed[n], completedCount = 0;
+    float currentTime = 0;
+    for (int i = 0; i < n; i++) completed[i] = 0;
+    
+    while (completedCount < n) {
+        int minIndex = -1, highestPriority = INT_MAX;
+        
+        for (int i = 0; i < n; i++) {
+            if (!completed[i] && at[i] <= currentTime) {
+                if (minIndex == -1 || priority[i] < highestPriority || (priority[i] == highestPriority && at[i] < at[minIndex])) {
+                    highestPriority = priority[i];
+                    minIndex = i;
                 }
             }
         }
-
-        if (idle == 0 && found == 0) {
-            // CPU is idle if no process has arrived
-            strcpy(ganttChart[num], "Idle");
-            startTime[num] = i;
-            i++;
-            idle = 1;
-        } else if (found == 1) {
-            if (idle == 1) {
-                endTime[num] = i;
-                num++;
-                idle = 0;
-            }
-
-            // Execute process
-            strcpy(ganttChart[num], name[min]);
-            status[min] = 1;
-            startTime[num] = i;
-            endTime[num] = i + burstTime[min];
-
-            // Update process times
-            i = endTime[num];
-            completionTime[min] = endTime[num];
-            turnAroundTime[min] = completionTime[min] - arrivalTime[min];
-            waitingTime[min] = turnAroundTime[min] - burstTime[min];
-
-            num++;
-            lastScheduled++;
+        
+        if (minIndex == -1) {
+            currentTime++;
         } else {
-            i++;
+            start[minIndex] = currentTime;
+            wt[minIndex] = currentTime - at[minIndex];
+            currentTime += bt[minIndex];
+            ct[minIndex] = currentTime;
+            completed[minIndex] = 1;
+            completedCount++;
         }
     }
+}
 
-    // Output Results
-    printf("\nProcess Name\tCompletion Time (ms)\tWaiting Time (ms)\tTurnaround Time (ms)\n\n");
-    for (i = 0; i < n; i++) {
-        printf("%s\t\t%d\t\t\t%d\t\t\t%d\n", name[i], completionTime[i], waitingTime[i], turnAroundTime[i]);
-        totalWaitingTime += waitingTime[i];
-        totalTurnAroundTime += turnAroundTime[i];
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
     }
+}
 
-    // Print Gantt Chart
-    printf("\n\nGANTT CHART ");
-    printf("\n\t------------------------------------------------------\n\t");
-    for (i = 0; i < num; i++) {
-        printf("| %s ", ganttChart[i]);
+void findAvgTime(int processes[], int n, int bt[], float at[], int priority[]) {
+    int wt[n], tat[n], ct[n], start[n];
+    findWaitingTime(processes, n, bt, at, priority, wt, ct, start);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    int total_wt = 0, total_tat = 0;
+    printf("\nProcess\tArrival Time\tBurst Time\tPriority\tWaiting Time\tTurnaround Time\tCompletion Time\n");
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t\t%.1f\t\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", processes[i], at[i], bt[i], priority[i], wt[i], tat[i], ct[i]);
     }
-    printf("|");
-    printf("\n\t------------------------------------------------------\n\t");
-    for (i = 0; i < num; i++) {
-        printf("%d\t", startTime[i]);
+    printf("\nAverage Waiting Time: %.2f", (float)total_wt / n);
+    printf("\nAverage Turnaround Time: %.2f\n");
+}
+
+void printGanttChart(int processes[], int n, int bt[], float at[], int priority[]) {
+    int completed[n], completedCount = 0, start[n], ct[n];
+    float currentTime = 0;
+    for (int i = 0; i < n; i++) completed[i] = 0;
+    
+    printf("\nGantt Chart:\n");
+    while (completedCount < n) {
+        int minIndex = -1, highestPriority = INT_MAX;
+        
+        for (int i = 0; i < n; i++) {
+            if (!completed[i] && at[i] <= currentTime) {
+                if (minIndex == -1 || priority[i] < highestPriority || (priority[i] == highestPriority && at[i] < at[minIndex])) {
+                    highestPriority = priority[i];
+                    minIndex = i;
+                }
+            }
+        }
+        
+        if (minIndex == -1) {
+            printf("| Idle (%d) ", (int)currentTime);
+            currentTime++;
+        } else {
+            if (currentTime < at[minIndex]) {
+                printf("| Idle (%d) ", (int)currentTime);
+                currentTime = at[minIndex];
+            }
+            printf("| P%d (%d) ", processes[minIndex], (int)currentTime);
+            start[minIndex] = currentTime;
+            currentTime += bt[minIndex];
+            ct[minIndex] = currentTime;
+            completed[minIndex] = 1;
+            completedCount++;
+        }
     }
-    printf("%d\n", endTime[num - 1]);
+    printf("|\n");
+}
 
-    // Print Average Times
-    printf("\nAverage Waiting Time: %.2f ms", totalWaitingTime / n);
-    printf("\nAverage Turnaround Time: %.2f ms\n", totalTurnAroundTime / n);
-
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    
+    int processes[n], burst_time[n], priority[n];
+    float arrival_time[n];
+    printf("Enter arrival times, burst times, and priority for each process:\n");
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1;
+        printf("Arrival time for Process %d: ", processes[i]);
+        scanf("%f", &arrival_time[i]);
+        printf("Burst time for Process %d: ", processes[i]);
+        scanf("%d", &burst_time[i]);
+        printf("Priority for Process %d: ", processes[i]);
+        scanf("%d", &priority[i]);
+    }
+    
+    printGanttChart(processes, n, burst_time, arrival_time, priority);
+    findAvgTime(processes, n, burst_time, arrival_time, priority);
+    
     return 0;
 }
