0b250fae661a260424dc903edc7836786cbffcb4
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2025-04-10 11:33:12 +0530

    Update sjf.c

--- Scheduling /Non_preemptive /sjf.c
+++ Scheduling /Non_preemptive /sjf.c
@@ -1,41 +1,38 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, j, temp, minIndex;
-    int id[20], arrivalTime[20], burstTime[20];
-    int completionTime[20], turnAroundTime[20], waitingTime[20];
-    float totalTurnAroundTime = 0, totalWaitingTime = 0;
-    int isCompleted[20] = {0};
-
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-
-    for (i = 0; i < n; i++) {
+void input(int n, int id[], int arrivalTime[], int burstTime[]) {
+    for (int i = 0; i < n; i++) {
         id[i] = i + 1;
         printf("\nEnter Arrival Time and Burst Time for Process %d: ", id[i]);
         scanf("%d %d", &arrivalTime[i], &burstTime[i]);
     }
+}
 
-    // Sorting by Arrival Time
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
+void sortByArrivalTime(int n, int id[], int arrivalTime[], int burstTime[]) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (arrivalTime[j] > arrivalTime[j + 1]) {
-                temp = arrivalTime[j]; 
-                arrivalTime[j] = arrivalTime[j + 1]; 
+                temp = arrivalTime[j];
+                arrivalTime[j] = arrivalTime[j + 1];
                 arrivalTime[j + 1] = temp;
-                
-                temp = burstTime[j]; 
-                burstTime[j] = burstTime[j + 1]; 
+
+                temp = burstTime[j];
+                burstTime[j] = burstTime[j + 1];
                 burstTime[j + 1] = temp;
-                
-                temp = id[j]; 
-                id[j] = id[j + 1]; 
+
+                temp = id[j];
+                id[j] = id[j + 1];
                 id[j + 1] = temp;
             }
         }
     }
+}
 
-    int currentTime = 0, completed = 0;
+void calculateSJF(int n, int id[], int arrivalTime[], int burstTime[],
+                  int completionTime[], int turnAroundTime[], int waitingTime[],
+                  int isCompleted[], float totalTurnAroundTime[], float totalWaitingTime[]) {
+    int currentTime = 0, completed = 0, minIndex, i;
 
     printf("\nGantt Chart:\n|");
 
@@ -58,8 +55,8 @@ int main() {
             turnAroundTime[minIndex] = completionTime[minIndex] - arrivalTime[minIndex];
             waitingTime[minIndex] = turnAroundTime[minIndex] - burstTime[minIndex];
 
-            totalTurnAroundTime += turnAroundTime[minIndex];
-            totalWaitingTime += waitingTime[minIndex];
+            totalTurnAroundTime[0] += turnAroundTime[minIndex];
+            totalWaitingTime[0] += waitingTime[minIndex];
 
             isCompleted[minIndex] = 1;
             completed++;
@@ -67,14 +64,39 @@ int main() {
             printf(" P%d |", id[minIndex]);
         }
     }
+}
 
-    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
+void display(int n, int id[], int arrivalTime[], int burstTime[], int completionTime[],
+             int turnAroundTime[], int waitingTime[], float totalTurnAroundTime, float totalWaitingTime) {
+    int i;
+    printf("\n\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
     for (i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", id[i], arrivalTime[i], burstTime[i], completionTime[i], turnAroundTime[i], waitingTime[i]);
+        printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",
+               id[i], arrivalTime[i], burstTime[i],
+               completionTime[i], turnAroundTime[i], waitingTime[i]);
     }
 
     printf("\nAverage Turnaround Time: %.2f", totalTurnAroundTime / n);
     printf("\nAverage Waiting Time: %.2f\n", totalWaitingTime / n);
+}
+
+int main() {
+    int n, i;
+    int id[20], arrivalTime[20], burstTime[20];
+    int completionTime[20], turnAroundTime[20], waitingTime[20];
+    int isCompleted[20] = {0};
+    float totalTurnAroundTime[1] = {0}, totalWaitingTime[1] = {0};
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    input(n, id, arrivalTime, burstTime);
+    sortByArrivalTime(n, id, arrivalTime, burstTime);
+    calculateSJF(n, id, arrivalTime, burstTime, completionTime,
+                 turnAroundTime, waitingTime, isCompleted,
+                 totalTurnAroundTime, totalWaitingTime);
+    display(n, id, arrivalTime, burstTime, completionTime,
+            turnAroundTime, waitingTime, totalTurnAroundTime[0], totalWaitingTime[0]);
 
     return 0;
 }
