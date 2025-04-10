d99e9a6061d7ec4fa06c09c83384c089e201b806
Mathew Joseph T A <mathewjoseph3831v@gmail.com>
2025-04-10 11:31:54 +0530

    Update fcfs.c

--- Scheduling /Non_preemptive /fcfs.c
+++ Scheduling /Non_preemptive /fcfs.c
@@ -1,70 +1,82 @@
 #include <stdio.h>
 
-int main() {
-    int n, i, j, temp;
-    int id[20], arrivalTime[20], burstTime[20];
-    int completionTime[20], turnAroundTime[20], waitingTime[20];
-    float totalTurnAroundTime = 0, totalWaitingTime = 0;
-
-    printf("Enter number of processes: ");
-    scanf("%d", &n);
-
-    // Input process details
-    for (i = 0; i < n; i++) {
+void inputProcesses(int n, int id[], int arrivalTime[], int burstTime[]) {
+    for (int i = 0; i < n; i++) {
         id[i] = i + 1;
         printf("\nEnter Arrival Time and Burst Time for Process %d: ", id[i]);
         scanf("%d %d", &arrivalTime[i], &burstTime[i]);
     }
+}
 
-    // Sorting by Arrival Time (Bubble Sort inside main)
-    for (i = 0; i < n - 1; i++) {
-        for (j = 0; j < n - i - 1; j++) {
+void sortByArrivalTime(int n, int id[], int arrivalTime[], int burstTime[]) {
+    int temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (arrivalTime[j] > arrivalTime[j + 1]) {
-                // Swap Arrival Time
+                // Swap arrival time
                 temp = arrivalTime[j];
                 arrivalTime[j] = arrivalTime[j + 1];
                 arrivalTime[j + 1] = temp;
 
-                // Swap Burst Time
+                // Swap burst time
                 temp = burstTime[j];
                 burstTime[j] = burstTime[j + 1];
                 burstTime[j + 1] = temp;
 
-                // Swap Process ID
+                // Swap process ID
                 temp = id[j];
                 id[j] = id[j + 1];
                 id[j + 1] = temp;
             }
         }
     }
+}
 
-    // Calculate Completion Time dynamically
-    int currentTime = 0;  // Tracks the CPU time
-    for (i = 0; i < n; i++) {
-        if (currentTime < arrivalTime[i]) {  // If CPU is idle
+void calculateTimes(int n, int arrivalTime[], int burstTime[], int completionTime[],
+                    int turnAroundTime[], int waitingTime[], float *totalTAT, float *totalWT) {
+    int currentTime = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (currentTime < arrivalTime[i]) {
             currentTime = arrivalTime[i];
         }
+
         completionTime[i] = currentTime + burstTime[i];
         currentTime = completionTime[i];
 
-        // Calculate Turnaround Time & Waiting Time
         turnAroundTime[i] = completionTime[i] - arrivalTime[i];
         waitingTime[i] = turnAroundTime[i] - burstTime[i];
 
-        totalTurnAroundTime += turnAroundTime[i];
-        totalWaitingTime += waitingTime[i];
+        *totalTAT += turnAroundTime[i];
+        *totalWT += waitingTime[i];
     }
+}
 
-    // Display Results
+void displayResults(int n, int id[], int arrivalTime[], int burstTime[], int completionTime[],
+                    int turnAroundTime[], int waitingTime[], float totalTAT, float totalWT) {
     printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tTurnaround Time\tWaiting Time\n");
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         printf("%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n", id[i], arrivalTime[i], burstTime[i],
                completionTime[i], turnAroundTime[i], waitingTime[i]);
     }
 
-    // Display Average Times
-    printf("\nAverage Turnaround Time: %.2f", totalTurnAroundTime / n);
-    printf("\nAverage Waiting Time: %.2f\n", totalWaitingTime / n);
+    printf("\nAverage Turnaround Time: %.2f", totalTAT / n);
+    printf("\nAverage Waiting Time: %.2f\n", totalWT / n);
+}
+
+int main() {
+    int n;
+    int id[20], arrivalTime[20], burstTime[20];
+    int completionTime[20], turnAroundTime[20], waitingTime[20];
+    float totalTAT = 0, totalWT = 0;
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    inputProcesses(n, id, arrivalTime, burstTime);
+    sortByArrivalTime(n, id, arrivalTime, burstTime);
+    calculateTimes(n, arrivalTime, burstTime, completionTime, turnAroundTime, waitingTime, &totalTAT, &totalWT);
+    displayResults(n, id, arrivalTime, burstTime, completionTime, turnAroundTime, waitingTime, totalTAT, totalWT);
 
     return 0;
 }
